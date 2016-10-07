52f73ae1b5e3b4c12e173965a2a84b3c904ef46e
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2016-10-07 22:45:24 +0000

    CLNDR template
    Moves template to own file and changes calendar from `div`s to a `table`
    (a calendar is tabular data).

--- js/myclndr.js
+++ js/myclndr.js
@@ -1,15 +1,28 @@
-$('.clndr').clndr({
-    events: [
-    {
-        "date": Date.now(),
-        "title": "Test",
-    },
-    ],
-    template: $("#clndr-tmpl").html(),
-    clickEvents: {
-        click: function(target){
-            console.log(target);
+$.get('clndr-template.html', function(data){
+    $('.clndr').clndr({
+        events: [
+            {
+                "date": Date.now(),
+                "title": "Test",
+            },
+            {
+                end: '2016-10-20',
+                start: '2016-10-15',
+                title: 'Another Long Event'
+            },
+        ],
+        template: data,
+        clickEvents: {
+            click: function(target){
+                console.log(target);
+            },
         },
-    },
+        forceSixRows: true,
+        multiDayEvents: {
+            endDate: 'end',
+            singleDay: 'date',
+            startDate: 'start'
+        },
 
+    });
 });
