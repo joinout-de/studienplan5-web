71f93ad5f61b2d3802f7b0b433cdabe9f0193a96
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2016-12-03 17:54:03 +0100

    Templating

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ var gulp = require('gulp');
 var sftp = require('gulp-sftp');
 var wiredep = require('wiredep').stream;
 var mainBowerFiles = require('main-bower-files');
+
+// gulp-handlebars
+var handlebars = require('gulp-handlebars');
+var wrap = require('gulp-wrap');
+var declare = require('gulp-declare');
+var concat = require('gulp-concat');
+
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
@@ -12,18 +19,29 @@ var files = ["*.html", "css/*.css", "js/*.js"].concat(mainBowerFiles());
 var fonts = [ bc + "bootstrap/dist/fonts/*" ];
 
 // bower task handles index.html
-gulp.task('default', ['fonts'], function() {
+gulp.task('default', ['fonts', 'templates'], function() {
     gulp.src(files, {base: '.'})
     .pipe(wiredep())
     .pipe(gulp.dest(dest));
 });
-
 
 gulp.task('fonts', function(){
     gulp.src(fonts, {base: '.'})
     .pipe(gulp.dest(dest));
 })
 
+// 99% https://github.com/lazd/gulp-handlebars#gulpfilejs
+gulp.task('templates', function(){
+  gulp.src('templates/*.hbs')
+    .pipe(handlebars())
+    .pipe(wrap('Handlebars.template(<%= contents %>)'))
+    .pipe(declare({
+      namespace: 'Templates',
+      noRedeclare: true, // Avoid duplicate declarations
+    }))
+    .pipe(concat('templates.js'))
+    .pipe(gulp.dest(dest, { base: '.' }));
+});
 
 gulp.task('clean', function() {
 });
@@ -36,7 +54,7 @@ gulp.task('serve', ["default"], function() {
     }
   });
 
-  gulp.watch([files], {cwd: "."}, ["reload"]);
+  gulp.watch([files].concat(['templates/*.hbs']), {cwd: "."}, ["reload"]);
 });
 
 gulp.task('reload', ["default"], function() {
